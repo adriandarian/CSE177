2db9cacd3a755b10114d487bceffd0dd4dc87c7a
Adrian Darian <adarian@ucmerced.edu>
2019-02-06 13:30:33 -0800

    db success

--- Phase1/code/Catalog.cc
+++ Phase1/code/Catalog.cc
@@ -8,7 +8,18 @@ using namespace std;
 
 
 Catalog::Catalog(string& _fileName) {
-	
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	rc = sqlite3_open("test.db", &db);
+
+	if( rc ) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	} else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	sqlite3_close(db);
 }
 
 Catalog::~Catalog() {
