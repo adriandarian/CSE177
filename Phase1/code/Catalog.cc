88d48124dc5e55fb4988463ba20893835da4dc5a
artymiss <artyspam@gmail.com>
2019-02-12 00:33:46 -0800

    Version control??

--- Phase1/code/Catalog.cc
+++ Phase1/code/Catalog.cc
@@ -8,24 +8,25 @@
 using namespace std;
 
 
+//TODO: I don't think this garbo is even needed
 struct AttsStruct {
 	string name;
 	string type;
-	int noDistinct;
+	unsigned int noDistinct;
 };
 
 struct TablesStruct {
 	string name;
 	string pathToFile;
-	//AttsStruct atts;
-	int noTuples;
+	Schema schema;
+	unsigned int noTuples;
 };
 
 vector<TablesStruct> tablesList;
-vector<string> attsList;
+vector<AttsStruct> attsList;
 vector<string> pathToFile;
-vector<int> noTuples;
-vector<int> noDistinct;
+vector<unsigned int> noTuples;
+vector<unsigned int> noDistinct;
 vector<Schema> schemaList; // Not sure if this is needed
 
 
@@ -48,14 +49,25 @@ Catalog::~Catalog() {
 }
 
 bool Catalog::Save() {
+	//Update the database boi
 	return true;
 }
 
 bool Catalog::GetNoTuples(string& _table, unsigned int& _noTuples) {
-	return true;
+	//TODO: Check mate (esp return false)
+	TablesStruct tab;
+	for(auto it = tablesList.begin(); it != tablesList.end(); it++) {
+		tab = *it;
+		if(tab.name == _table) {
+			_noTuples = tab.noTuples;
+			return true;
+		}
+	}
+	return false;
 }
 
 void Catalog::SetNoTuples(string& _table, unsigned int& _noTuples) {
+	//TODO: Check mate
 	//Loop through list of tables and delete
 	TablesStruct tab;
 	for(auto it = tablesList.begin(); it != tablesList.end(); it++) {
@@ -69,30 +81,98 @@ void Catalog::SetNoTuples(string& _table, unsigned int& _noTuples) {
 }
 
 bool Catalog::GetDataFile(string& _table, string& _path) {
-	return true;
+	//TODO: Check mate (esp return false)
+	TablesStruct tab;
+	for(auto it = tablesList.begin(); it != tablesList.end(); it++) {
+		tab = *it;
+		if(tab.name == _table) {
+			_path = tab.pathToFile;
+			return true;
+		}
+	}
+
+	return false;
 }
 
 void Catalog::SetDataFile(string& _table, string& _path) {
+	//TODO: Check mate
+	TablesStruct tab;
+	for(auto it = tablesList.begin(); it != tablesList.end(); it++) {
+		tab = *it;
+		if(tab.name == _table) {
+			tab.pathToFile = _path;
+			*it = tab;
+		}
+	}
 }
 
 bool Catalog::GetNoDistinct(string& _table, string& _attribute, unsigned int& _noDistinct) {
-	return true;
+	TablesStruct tab;
+	for(auto it = tablesList.begin(); it != tablesList.end(); it++) {
+		tab = *it;
+		if(tab.name == _table) {
+			for(int i = 0; i < tab.schema.GetAtts().size(); ++i) {
+				if(tab.schema.GetAtts()[i].name == _attribute) {
+					_noDistinct = tab.schema.GetDistincts(_attribute);
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
 void Catalog::SetNoDistinct(string& _table, string& _attribute, unsigned int& _noDistinct) {
+		//TODO: Figure out what in the heck to put here
+	TablesStruct tab;
+	for(auto it = tablesList.begin(); it != tablesList.end(); ++it) {
+		tab = *it;
+		if(tab.name == _table) {
+			for(int i = 0; i < tab.schema.GetAtts().size(); ++i) {
+				if(tab.schema.GetAtts()[i].name == _attribute) {
+					//_noDistinct = tab.schema.GetDistincts(_attribute);
+				}
+			}
+		}
+	}
 }
 
 void Catalog::GetTables(vector<string>& _tables) {
+	//TODO: Can we just push_back straight to _tables?
+	TablesStruct tab;
+	for(auto it = tablesList.begin(); it != tablesList.end(); ++it) {
+		tab = *it;
+		_tables.push_back(tab.name);
+	}
 }
 
 bool Catalog::GetAttributes(string& _table, vector<string>& _attributes) {
+	//TODO: Implement return false
+	TablesStruct tab;
+	vector<string> newAtts;
+	for(auto it = tablesList.begin(); it != tablesList.end(); it++) {
+		tab = *it;
+		if(tab.name == _table) {
+			for(int i = 0; i < tab.schema.GetAtts().size(); ++i) {
+				_attributes.push_back(tab.schema.GetAtts()[i].name);
+				//TODO: Is this needed?
+				//newAtts.push_back(tab.schema.GetAtts()[i].name);
+			}
+			
+		}
+	}
+	//_attributes = newAtts;
 	return true;
 }
 
 bool Catalog::GetSchema(string& _table, Schema& _schema) {
-	//Loop through all tables
-	//Once _table is found set _schema to corresponding schema to that tables
-	//return true;
-	//else return false
+	//Hashmap of schema and table?
+	TablesStruct tab;
+	for(auto it = tablesList.begin(); it != tablesList.end(); ++it;) {
+		tab = *it;
+		if(tab.name == _table) {
+			_schema = tab.schema;
+		}
+	}
 	return true;
 }
 
@@ -100,34 +180,54 @@ bool Catalog::CreateTable(string& _table, vector<string>& _attributes, vector<st
 	//Check for a valid attribute type
 	//Integer in main-2.cc INTEGER in main.cc
 	for(int i = 0; i < _attributeTypes.size(); ++i) {
+		//TODO: Prettify this
 		if(_attributeTypes[i] != "Integer" || _attributeTypes[i] != "Float" || _attributeTypes[i] != "String")
 			return false;
+	}
+
+	//Check for duplicate tables
+	unordered_set <string> tablesHash;
+	if(tablesHash.find(_table) != tablesHash.end()) { // Found duplicate
+		return false;
+	}
+	else { //Not Found
+		tablesHash.insert(_table);
 	}
 
 	//Check for duplicate attributes, Return false if duplicate is found
 	unordered_set <string> attsHash;
 	for(int i = 0; i < _attributes.size(); ++i) {
-		if(attsHash.find(_attributes[i]) != attsHash.end()) {
+		if(attsHash.find(_attributes[i]) != attsHash.end()) { //Found duplicate
 			return false;
 		}
-		else {
+		else { //Not Found
+			//TODO: Add atts here instead
 			attsHash.insert(_attributes[i]);
 		}
 	}
-	//Check for duplicate tables
-	unordered_set <string> tablesHash;
-		if(tablesHash.find(_table) != tablesHash.end()) {
-			return false;
-		}
-		else {
-			tablesHash.insert(_table);
-		}
+	//TODO: Ok this bullshit is way uglier than it needs to be. Simplify dum-dum
+	//Add new table
 	TablesStruct newTable;
+	AttsStruct newAtt;
+	
 	newTable.name = _table;
+	//Do I even set these or just keep at null
 	newTable.noTuples = 0;
 	newTable.pathToFile="NoPath";
-	tablesList.push_back(newTable);
+	//Have to list through total atts for the table
+	for(int i = 0; i < _attributes.size(); ++i) {
+		noDistinct.push_back(0); //For schema assigning
+		newAtt.name = _attributes[i];
+		newAtt.type = _attributeTypes[i];
+		newAtt.noDistinct = 0;
+		attsList.push_back(newAtt);
+	}
+    Schema newSchema(_attributes, _attributeTypes, noDistinct);
 
+	newTable.schema = newSchema;
+	tablesList.push_back(newTable);
+	//Need a way to tie schema and table - Done. Added schema to TablesStruct - removed Atts
+	//schemaList.push_back(Schema(_attributes, _attributeTypes, noDistinct));
 	return true;
 }
 
